0c9ca8f62b954b92e8b1cec0c2562b83e6bd095e
mariahseggebruch <mariahseggebruch@gmail.com>
2023-05-20 10:29:41 -0700

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -34,3 +34,12 @@ class Square():
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def my_print(self):
+        if(self.__size <= 0):
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
