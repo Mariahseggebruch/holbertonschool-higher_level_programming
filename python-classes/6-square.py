4801bed2ff8711038b15ecc7a0ed968fe981cdb4
mariahseggebruch <mariahseggebruch@gmail.com>
2023-05-20 10:32:46 -0700

    6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 4-square.py)
+
+Private instance attribute: size:
+property def size(self): to retrieve it
+property setter def size(self, value): to set it:
+size must be an integer, otherwise raise a TypeError exception with a message
+if size is less than 0, raise a ValueError exception with a message
+Instantiation with optional size: def __init__(self, size=0):
+Public instance method: def area(self): that returns the current square area
+Public instance method: def my_print(self): that prints in stdout
+if size is equal to 0, print an empty line
+You are not allowed to import any module
+"""
