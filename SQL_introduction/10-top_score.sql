55204dc7eb8e06c07e5b7b3b4e24def7dea1ad21
Mariahseggebruch <mariahseggebruch@gmail.com>
2023-06-28 01:52:42 +0000

    final tasks

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- 10-top_score.sql, Mariah Seggebruch C20
+-- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT score, name FROM second_table ORDER BY score DESC;
