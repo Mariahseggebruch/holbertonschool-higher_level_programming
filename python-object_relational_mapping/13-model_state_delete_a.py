cfdb31772bfd19b58ca2ae3a5458ba93b15f21a2
Mariahseggebruch <mariahseggebruch@gmail.com>
2023-06-28 20:47:50 +0000

    final tasks initial commit

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that deletes all State objects
+    with name containing the letter 'a'
+    from the database hbtn_0e_6_usa"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create SQLAlchemy engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)  # checks if connection invalid
+
+    # Create session factory bound to the engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.like('%a%')) \
+                        .order_by(State.id):
+        session.delete(state)
+
+    session.commit()
+    session.close()
