72f725c92289e8db7779e9cc45eca344a814cf52
Mariahseggebruch <117305588+Mariahseggebruch@users.noreply.github.com>
2023-06-28 10:59:01 -0500

    Delete 0-privileges.sql
    Re added file with correct spacing

--- SQL_more_queries/ 0-privileges.sql
+++
@@ -1,4 +0,0 @@
--- 0-privileges.sql, Mariah Seggebruch C20
--- Script that lists all privileges of the MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
