439489fc40e1d67fc9700c440b7f76d40cf3cbce
Mariahseggebruch <mariahseggebruch@gmail.com>
2023-06-28 15:56:44 +0000

    task 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- 0-privileges.sql, Mariah Seggebruch C20
+-- Script that lists all privileges of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
